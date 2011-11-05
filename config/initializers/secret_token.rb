a74830fd6e3464ae20aa9ba894c022c216156313
Yuki Ueda <ueda@tos.co.jp>
2011-11-06 00:07:50 +0900

    default rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gijiroku::Application.config.secret_token = '03535cf23c02d7d18e2840f8e2b670d983209e59e254d5b4b058acc5fbd608d5862c54d3691539cff705eb7fc427ff0d2e55ab44e01911ffef2fc9b82e7c759f'
