589790bf070c722ce28c313a0f19cbf76320bb58
Yuki Ueda <ueda@tos.co.jp>
2011-11-12 00:38:28 +0900

    adjusted assets and session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-Gijiroku::Application.config.session_store :cookie_store, key: '_Gijiroku_session'
+#Gijiroku::Application.config.session_store :cookie_store, key: '_Gijiroku_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Gijiroku::Application.config.session_store :active_record_store
+Gijiroku::Application.config.session_store :active_record_store
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111110145313) do
+ActiveRecord::Schema.define(:version => 20111111144655) do
 
   create_table "articles", :force => true do |t|
     t.text     "content"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(:version => 20111110145313) do
     t.datetime "updated_at"
     t.string   "attendee"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "types", :force => true do |t|
     t.string   "name"
