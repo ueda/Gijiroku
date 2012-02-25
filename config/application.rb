73f98b111191c79c3f794aabb4c577cc3b67a43c
Yuki Ueda <ueda@tos.co.jp>
2012-02-26 00:24:51 +0900

    added icon stylesheet etc.

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module Gijiroku
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Type.create(:name=>"Ok!", :icon_filename=>"icon_ok.png")
+Type.create(:name=>"Ok", :icon_filename=>"icon_ok.png")
 Type.create(:name=>"Info", :icon_filename=>"icon_info.png")
 Type.create(:name=>"ToDo", :icon_filename=>"icon_todo.png")
 Type.create(:name=>"Problem", :icon_filename=>"icon_problem.png")
