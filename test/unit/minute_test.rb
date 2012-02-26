1c0ea539ad3796664d52a2f5c16b266be76b0088
Yuki Ueda <ueda@tos.co.jp>
2012-02-26 23:38:32 +0900

    added some fixture and unit test script for minute model.
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   config/database.yml
      modified:   test/fixtures/articles.yml
      modified:   test/fixtures/minutes.yml
    	modified:   test/fixtures/types.yml
    	modified:   test/fixtures/users.yml
    	modified:   test/unit/minute_test.rb

--- test/unit/minute_test.rb
+++ test/unit/minute_test.rb
@@ -4,4 +4,20 @@ class MinuteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save minute without title" do
+    #minute = Minute.new
+    minute = minutes(:mtg01)
+    assert !minute.save
+  end
+
+  test "should save minute with title" do
+    minute = minutes(:mtg02)
+    assert minute.save
+  end 
+
+  test "should save minutes with created_by and updated_by as null" do
+    minute = minutes(:mtg02)
+    minute.save
+    assert minute.created_by.nil? && minute.updated_by.nil?
+  end
 end
